43eb8bc2ccda4495f05a0d6ed496efae97b827ad
Ian Mejias <ian.mejias@usach.cl>
2015-12-02 03:30:54 -0300

    Se elimina la tabla intermedia `encuesta_pregunta`
    No tenía ninguna utilidad dentro de la BD haciendo que se tenga que
    recorrer una tabla adicional al querer saber las preguntas de cierta
    encuesta.

--- app/models/encuesta_preguntum.rb
+++
@@ -1,4 +0,0 @@
-class EncuestaPreguntum < ActiveRecord::Base
-	belongs_to :encuestum
-	belongs_to :preguntum
-end
--- db/migrate/20151128024434_create_encuesta_pregunta.rb
+++
@@ -1,9 +0,0 @@
-class CreateEncuestaPregunta < ActiveRecord::Migration
-  def change
-    create_table :encuesta_pregunta do |t|
-      t.integer :Encuesta_Pregunta_id
-
-      t.timestamps null: false
-    end
-  end
-end

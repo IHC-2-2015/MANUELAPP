25d5ae9cb435930099339b1f7aa6f97e72e141b3
Ian Mejias <ian.mejias@usach.cl>
2015-12-02 04:40:41 -0300

    ADD entidad Profesor

---
+++ app/models/profesor.rb
@@ -0,0 +1,3 @@
+class Profesor < ActiveRecord::Base
+  self.table_name = "profesores"
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151128024744) do
+ActiveRecord::Schema.define(version: 20151202073639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,15 @@ ActiveRecord::Schema.define(version: 20151128024744) do
     t.string   "enunciado"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "profesores", force: :cascade do |t|
+    t.string   "nombre"
+    t.string   "apellido"
+    t.text     "descripcion"
+    t.integer  "usuario_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "respuestas", force: :cascade do |t|
